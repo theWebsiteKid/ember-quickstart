66fcb8083de485a159bde6d56fae77d3883ac85b
Xavier Duncan <xduncan@pm.me>
2018-10-01 11:53:00 -0400

    render data to programmers template

--- app/routes/programmers.js
+++ app/routes/programmers.js
@@ -2,6 +2,6 @@ import Route from '@ember/routing/route';
 
 export default Route.extend({
     model() {
-        return ['Guido van Rossum', 'Yukihiro Matsumoto', 'Linus Torvalds']
+        return ['Guido van Rossum', 'Yukihiro Matsumoto', 'Linus Torvalds'];
     }
 });
